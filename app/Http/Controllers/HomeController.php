8a405f1c8ca46cae02cd91de16907f0a00aa6226
abencic <antun.bencic@gmail.com>
2018-06-18 21:12:00 +0200

    Add home view and send data from controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class HomeController extends Controller
 {
@@ -13,6 +14,7 @@ class HomeController extends Controller
      */
     public function __invoke()
     {
-        return view('welcome');
+		$posts = Post::all();
+        return view('home', ['posts' => $posts]);
     }
 }
